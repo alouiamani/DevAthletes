a8fe1987ec97a767460999d0c118a8998a2e7dc3
wajd <97837888+Wajdbenameur1@users.noreply.github.com>
2025-02-13 10:36:27 +0100

    v1

---
+++ Pi/src/main/java/org/gymify/services/PlanningService.java
@@ -0,0 +1,93 @@
+package org.gymify.services;
+
+import org.gymify.entities.Planning;
+import org.gymify.utiles.gymifyDataBase;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlanningService implements IService<Planning> {
+    private Connection con;
+    public PlanningService() {
+        con = gymifyDataBase.getInstance().getConnection();
+    }
+
+    @Override
+    public void Add(Planning planning) {
+        try{
+            PreparedStatement req=con.prepareStatement ("INSERT INTO `planning`(`dateDebut`, `description`, `title`, `dateFin`) VALUES (?,?,?,?)");
+            req.setDate(1,new java.sql.Date(planning.getdateDebut().getTime()));
+            req.setString(2,planning.getDescription());
+            req.setString(3,planning.getTitle());
+            req.setDate(4,new java.sql.Date(planning.getdateFin().getTime()));
+            req.executeUpdate();
+            System.out.println("Planning ajouté avec succès.");
+
+        }catch (SQLException e){
+            System.out.println("Erreur lors de l'ajout du planning : " + e.getMessage());
+
+        }
+
+
+    }
+
+    @Override
+    public void Update(Planning planning) {
+        try{
+            PreparedStatement req=con.prepareStatement ("UPDATE `planning` SET dateDebut=?,description=?,title=?,dateFin=? WHERE id=?");
+            req.setDate(1,new java.sql.Date(planning.getdateDebut().getTime()));
+            req.setString(2,planning.getDescription());
+            req.setString(3,planning.getTitle());
+            req.setDate(4,new java.sql.Date(planning.getdateFin().getTime()));
+            req.setInt(5,planning.getId());
+            req.executeUpdate();
+            System.out.println("Planning mis à jour avec succès.");
+
+        }catch (SQLException e){
+            System.out.println("Erreur lors de la mise à jour du planning : " + e.getMessage());
+
+        }
+
+    }
+
+    @Override
+    public void Delete(Planning planning) {
+        try{
+            PreparedStatement req= con.prepareStatement("DELETE FROM planning WHERE id=?");
+            req.setInt(1,planning.getId());
+            req.executeUpdate();
+            System.out.println("Planning supprimé avec succès.");
+
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la suppression du planning : " + e.getMessage());
+
+        }
+
+    }
+
+    @Override
+    public List<Planning> Display() {
+        List<Planning> plannings = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM planning";
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()) {
+                Planning planning = new Planning();
+                planning.setId(rs.getInt("id"));
+                planning.setdateDebut(rs.getDate("dateDebut"));
+                planning.setDescription(rs.getString("description"));
+                planning.setTitle(rs.getString("title"));
+                planning.setDateFin(rs.getDate("dateFin"));
+                plannings.add(planning);
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de l'affichage des plannings : " + e.getMessage());
+
+        }
+        return plannings;
+    }
+}
