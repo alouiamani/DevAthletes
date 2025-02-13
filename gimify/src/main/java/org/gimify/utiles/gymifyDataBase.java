faf60f7c319f0a0d2ff1d2e7d36c4d2bd9f1e21b
wajd <97837888+wajdbenameur1@users.noreply.github.com>
2025-02-13 12:04:00 +0100

    d

---
+++ gimify/src/main/java/org/gimify/utiles/gymifyDataBase.java
@@ -0,0 +1,34 @@
+package org.gimify.utiles;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class gymifyDataBase {
+    static gymifyDataBase instance;
+    final String URL = "jdbc:mysql://localhost:3306/gymify";
+    final String USER = "root";
+    final String PASSWORD = "";
+    Connection con ;
+    private gymifyDataBase() {
+        try {
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to database");
+
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static gymifyDataBase getInstance() {
+        if (instance == null) {
+            instance = new gymifyDataBase();
+        }
+        return instance;
+    }
+    public Connection getConnection() {
+        return con;
+    }
+}
