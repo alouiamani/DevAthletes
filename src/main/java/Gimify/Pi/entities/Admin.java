8372e0fe999da43bc568bc2c6fe0f0ff5db0194f
wajd <97837888+Wajdbenameur1@users.noreply.github.com>
2025-02-20 19:54:24 +0100

    Delete src/main directory

--- src/main/java/Gimify/Pi/entities/Admin.java
+++
@@ -1,7 +0,0 @@
-package Gimify.Pi.entities;
-
-public class Admin extends User {
-    public Admin(int id_User, String nom, String prenom, String password, String role, String email) {
-        super(id_User, nom, prenom, password, "Admin", email);
-    }
-}
