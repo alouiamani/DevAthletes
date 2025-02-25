c1c44ccc193f8b0db6ff19447f13d23d2e859893
wajd <97837888+Wajdbenameur1@users.noreply.github.com>
2025-02-25 11:26:59 +0100

    v1

---
+++ gymify/src/main/java/org/gymify/tests/MainApp.java
@@ -0,0 +1,29 @@
+package org.gymify.tests;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/plannings.fxml"));
+        //FXMLLoader loader = new FXMLLoader(getClass().getResource("/addActivity.fxml"));
+
+        Parent root = loader.load();
+        primaryStage.setTitle("DevAthletes");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+
+    }
+}
