0448f535898965eb2e518144bdb9150d23d1516b
GhabiJihen <GhabiJihen@users.noreply.github.com>
2025-02-27 15:43:27 +0100

    Jihen

--- gymify/src/main/java/org/gymify/entities/Sportif.java
+++ gymify/src/main/java/org/gymify/entities/Sportif.java
@@ -3,11 +3,11 @@ package org.gymify.entities;
 import java.util.Date;
 
 public class Sportif extends User {
-    public Sportif(int id_User, String nom, String prenom, String email, String password, Date dateNaissance, String image) {
-        super();
+    public Sportif(int id_User, String nom, String prenom, String email, String password, Date dateNaissance, String imageURL) {
+        super(String.valueOf(id_User), nom, prenom, email, password,dateNaissance, imageURL);
     }
 
-    public Sportif(String nom, String prenom, String email, String password, Date dateNaissance, String image) {
+    public Sportif(String nom, String prenom, String email, String password, Date dateNaissance, String imageURL) {
         super(nom, prenom, email, password, "Sportif");
     }
 
