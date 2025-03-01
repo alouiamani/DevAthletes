bffe988ae36f1734fd0d07d594215e0b321da3f7
alouiamani <alouiamani48@gmail.com>
2025-03-01 13:30:16 +0100

    n
    nn

--- gymify/src/main/java/org/gymify/controllers/ListeDesSallesUser.java
+++ gymify/src/main/java/org/gymify/controllers/ListeDesSallesUser.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ListeDesSallesUser {
+public class ListeDesSallesUser extends ProfileMembreController {
 
     @FXML
     private TextField searchField;
