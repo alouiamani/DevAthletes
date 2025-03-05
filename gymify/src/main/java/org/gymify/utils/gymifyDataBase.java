40b2a6b7a9ca75902eaf22af374437f8d4d6b82d
GhabiJihen <GhabiJihen@users.noreply.github.com>
2025-03-05 23:50:54 +0100

    jihen

--- gymify/src/main/java/org/gymify/utils/gymifyDataBase.java
+++ gymify/src/main/java/org/gymify/utils/gymifyDataBase.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class gymifyDataBase {
     static gymifyDataBase instance;
-    final String URL = "jdbc:mysql://localhost:3306/gymify";
+    final String URL = "jdbc:mysql://localhost:3306/project";
     final String USER = "root";
     final String PASSWORD = "";
     Connection con ;
